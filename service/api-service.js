e8deac22a9e4faa15926784362f3a41c0446394a
mfaizk <suneater1413@gmai.com>
2025-10-13 22:31:52 +0530

    react query setup complete

--- service/api-service.js
+++ service/api-service.js
@@ -6,35 +6,3 @@ export const api = axios.create({
   //     "Content-Type": "application/json",
   //   },
 });
-
-// api.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("access_token");
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
-
-// // Response interceptor (optional)
-// api.interceptors.response.use(
-//   (response) => response, // Return data as-is
-//   (error) => {
-//     // Handle errors globally
-//     if (error.response) {
-//       // Server responded with a status outside 2xx
-//       console.error("API Error:", error.response.data);
-//     } else if (error.request) {
-//       // No response received
-//       console.error("No response from server:", error.request);
-//     } else {
-//       // Something went wrong setting up the request
-//       console.error("Axios error:", error.message);
-//     }
-//     return Promise.reject(error);
-//   }
-// );
-
-// export default api;
