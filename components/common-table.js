8a1c94b00d2354be362618dee8030eb8e44b50b7
mfaizk <suneater1413@gmai.com>
2025-10-26 21:29:34 +0530

    minor crash fix

--- components/common-table.js
+++ components/common-table.js
@@ -7,22 +7,22 @@ export default function CommonTable({ columns, data }) {
         <thead className="bg-[#1a1a25] text-gray-400 uppercase text-xs">
           <tr>
             {columns.map((col) => (
-              <th key={col.key} className="px-4 py-3 font-medium">
-                {col.label}
+              <th key={col?.key} className="px-4 py-3 font-medium">
+                {col?.label}
               </th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {data.length > 0 ? (
-            data.map((row, idx) => (
+          {data?.length > 0 ? (
+            data?.map((row, idx) => (
               <tr
                 key={idx}
                 className="border-t border-gray-800 hover:bg-[#1a1a2a] transition"
               >
                 {columns.map((col) => (
-                  <td key={col.key} className="px-4 py-3">
-                    {row[col.key]}
+                  <td key={col?.key} className="px-4 py-3">
+                    {row[col?.key]}
                   </td>
                 ))}
               </tr>
